0ab306bdd1cd8bc3e9d8903060604d29364a0683
Marcelo Matos <107945179+marcelomatoss@users.noreply.github.com>
2024-06-05 21:39:56 -0300

    Update Cliente.java

--- codigo/src/Cliente.java
+++ codigo/src/Cliente.java
@@ -1,28 +1,28 @@
 public class Cliente {
 
-	private static int ultimoID;
-	private String nomeCliente;
-	private int idCliente;
+    private static int ultimoID;
+    private String nomeCliente;
+    private int idCliente;
 
-	static{
-		ultimoID = 0;
-	}
+    static {
+        ultimoID = 0;
+    }
 
-	public Cliente(String nome) {
-		if(nome.length() > 2)
-			nomeCliente = nome;	
-		else 
-			nomeCliente = "Cliente anônimo";
-		idCliente = ++ultimoID;
-	}
+    public Cliente(String nome) {
+        if (nome.length() > 2)
+            nomeCliente = nome;
+        else
+            nomeCliente = "Cliente anônimo";
+        idCliente = ++ultimoID;
+    }
 
-	@Override
-	public String toString(){
-		return String.format("Nome: %s - identificador: %d", nomeCliente, idCliente);
-	}
+    @Override
+    public String toString() {
+        return String.format("Nome: %s - identificador: %d", nomeCliente, idCliente);
+    }
 
-	@Override
-	public int hashCode(){
-		return idCliente;
-	}
+    @Override
+    public int hashCode() {
+        return idCliente;
+    }
 }
